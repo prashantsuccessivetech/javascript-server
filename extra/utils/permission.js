514b0698335127b13198d76c733ced7b07b8f227
Prashant kumar <prashant.kumar@prashantk.successivetech.com>
2020-10-21 18:42:47 +0530

    feat:create permission

---
+++ extra/utils/permission.js
@@ -0,0 +1,50 @@
+let d;
+let permissions =
+{
+'getUsers': {
+all: ['head-trainer'],
+read : ['trainee', 'trainer'],
+write : ['trainer'],
+Delete: [],
+},
+'getUser1': {
+all: ['head-trainer'],
+read : ['trainee', 'trainer'],
+write : ['trainer'],
+Delete: ['trainee'],
+}
+}
+const {getUsers,getUser1}= permissions;
+let f;
+function hasPermission(moduleName,role,permissionType)
+{
+const {all,read,write,Delete}=moduleName;
+f = all.includes(role)
+
+if(f==true)
+{
+return true
+}
+else
+{
+if(permissionType=="read")
+{
+f=read.includes(role)
+return f;
+}
+else if(permissionType=="write"){
+f=write.includes(role);
+return f;
+}
+else if(permissionType=="Delete"){
+f=Delete.includes(role)
+return f;
+}
+
+}
+
+}
+d = hasPermission(getUsers,"trainee","read");
+console.log(d);
+d = hasPermission(getUser1,"trainer","Delete");
+console.log(d);
