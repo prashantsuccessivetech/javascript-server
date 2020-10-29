4166dcb14f2ecea26a1d58b3ec7c7fb14168425e
prashantsuccessivetech <prashant.kumar@successive.tech>
2020-10-29 19:36:47 +0530

    feat:Setting of Routes Skeletons (REST API)

---
+++ src/Controllers/trainee/routes.ts
@@ -0,0 +1,14 @@
+import { Router } from 'express';
+
+
+import traineeController from './controller';
+
+const traineeRouter = Router();
+
+traineeRouter.route('/')
+.get(traineeController.get)
+.post(traineeController.create)
+.put(traineeController.update)
+.delete(traineeController.delete)
+
+export default traineeRouter;
