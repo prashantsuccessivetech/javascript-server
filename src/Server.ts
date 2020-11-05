ecce6d014102a6af8c73898e7d494743bce7a027
prashantsuccessivetech <prashant.kumar@successive.tech>
2020-11-05 16:36:49 +0530

    feat:intro to mongoose and promises

--- src/Server.ts
+++ src/Server.ts
@@ -2,6 +2,7 @@ import * as express from "express";
 import * as bodyParser from "body-parser";
 import { notFoundRoute, errorHandler } from './libs/routes';
 import router from './router';
+import Database from './libs/Database';
 
 class Server{
     app
@@ -47,16 +48,24 @@ class Server{
     public initBodyParser(){
         this.app.use(bodyParser.json());
     }
-    run(){
-        const {app, config:{PORT}}=this;
-        app.listen(PORT,(err)=>{
-            if (err) {
-                console.log( err );
-                
-            }
-            console.log(`App is running on port ${PORT}`);
-
+    run() {
+        const { app, config: { PORT } } = this;
+        Database.open('mongodb://localhost:27017/express-training')
+        .then((res) => {
+        console.log('Succesfully connected to Mongo');
+        app.listen( PORT, (err) => {
+        if (err) {
+        console.log(err);
+        }
+        else {
+        console.log(`App is running on port ${process.env.PORT}`);
+        Database.disconnect();
+        }
+        });
         })
-    }
+        .catch(err => console.log(err));
+        return this;
+        }
+        
 }
 export default Server;
