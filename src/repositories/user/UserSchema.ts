287bb722c7f87415d39eab7d8a7a2eedbbb19f38
prashantsuccessivetech <prashant.kumar@successive.tech>
2020-11-09 11:38:15 +0530

    feat:model setup with initial seed

---
+++ src/repositories/user/UserSchema.ts
@@ -0,0 +1,16 @@
+import * as mongoose from 'mongoose';
+
+class UserSchema extends mongoose.Schema {
+
+    constructor(collections: any) {
+        const baseSchema = Object.assign({
+            id: String,
+            name: String,
+            role: String,
+            password: String,
+        });
+        super(baseSchema, collections);
+    }
+
+}
+export default UserSchema;
